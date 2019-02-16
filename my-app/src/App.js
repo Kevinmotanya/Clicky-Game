bdc8bd98339ec8163bd7c1b49b14b6938d9ff56f
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-02-16 11:03:28 -0600

    rabb

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Wrapper from './components/Wrapper'
+import Navig from './components/Navig'
+import Title from './components/Title'
+import MyCard from './components/MyCard'
+
 class App extends Component {
+  state = {
+    message: "Click an image to begin!",
+    topScore: 0,
+    curScore: 0,
+    dogs: dogs,
+    unselectedDogs: dogs
+}
+
+componentDidMount() {
+}
+
+shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+selectDog = breed => {
+    const findDog = this.state.unselectedDogs.find(item => item.breed === breed);
+
+    if(findDog === undefined) {
+        // failure to select a new dog
+        this.setState({ 
+            message: "You guessed incorrectly!",
+            topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+            curScore: 0,
+            dogs: dogs,
+            unselectedDogs: dogs
+        });
+    }
+    else {
+        // success to select a new dog
+        const newDogs = this.state.unselectedDogs.filter(item => item.breed !== breed);
+        
+        this.setState({ 
+            message: "You guessed correctly!",
+            curScore: this.state.curScore + 1,
+            dogs: dogs,
+            unselectedDogs: newDogs
+        });
+    }
+
+    this.shuffleArray(dogs);
+};
+
   render() {
     return (
       <div className="App">
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  render() {
+    return (
+      <div>
+        <Navbar
+          score={this.state.score}
+        />
+        <Jumbotron />
+        <div className="wrapper">
+          {this.state.bobsburgers.map(fish => (
+            <FriendCard
+              imageClick={this.imageClick}
+              id={bobsburgers.id}
+              key={bobsburgers.id}
+              image={bobsburgers.image}
+            />
+          ))}
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
